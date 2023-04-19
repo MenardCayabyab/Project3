cb47a62d5f5f7f56abcf40c625975013320718ac
Menard <cayabyab.menard@ymail.com>
2023-04-19 02:00:51 -0700

    Organized and designed dashboard

--- project/app.py
+++ project/app.py
@@ -19,9 +19,8 @@ def dashboard():
     selected_pet_type = ''
     selected_primary_breed = ''
     
-    # Initialize the pet_types and primary_breeds variables
+    # Get the list of pet types
     pet_types = [row[0] for row in query_database("SELECT DISTINCT Type FROM project3")]
-    primary_breeds = [row[0] for row in query_database("SELECT DISTINCT PrimaryBreed FROM project3")]
     
     if request.method == 'POST':
         # Process the selected options from the dropdown menus
@@ -29,17 +28,26 @@ def dashboard():
         primary_breed = request.form['primary_breed']
         
         # Retrieve the matching data from the database
-        data = query_database("SELECT * FROM project3 WHERE Type = ? AND PrimaryBreed = ?", (pet_type, primary_breed))
+        data = query_database("SELECT ID, Name, Color, Sex, Size, DateOfBirth, ImpoundNumber, KennelNumber, AnimalID, IntakeDate, DaysInShelter, IntakeType, IntakeSubtype, OutcomeType, OutcomeSubtype, IntakeCondition, OutcomeCondition, IntakeJurisdiction, OutcomeJurisdiction, OutcomeZipCode, SecondaryBreed FROM project3 WHERE Type = ? AND PrimaryBreed = ?", (pet_type, primary_breed))
         
         # Set the selected options for the dropdown menus
         selected_pet_type = pet_type
         selected_primary_breed = primary_breed
         
+        # Get the list of primary breeds for the selected pet type
+        primary_breeds = [row[0] for row in query_database("SELECT DISTINCT PrimaryBreed FROM project3 WHERE Type = ?", (pet_type,))]
+        
         # Render the template with the matching data
         return render_template('dashboard.html', pet_types=pet_types, primary_breeds=primary_breeds, selected_pet_type=selected_pet_type, selected_primary_breed=selected_primary_breed, data=data)
     else:
+        # Get the list of primary breeds for the default pet type
+        primary_breeds = [row[0] for row in query_database("SELECT DISTINCT PrimaryBreed FROM project3 WHERE Type = ?", (pet_types[0],))]
+        
+        # Retrieve the matching data from the database for the default pet type and primary breed
+        data = query_database("SELECT ID, Name, Color, Sex, Size, DateOfBirth, ImpoundNumber, KennelNumber, AnimalID, IntakeDate, DaysInShelter, IntakeType, IntakeSubtype, OutcomeType, OutcomeSubtype, IntakeCondition, OutcomeCondition, IntakeJurisdiction, OutcomeJurisdiction, OutcomeZipCode, SecondaryBreed FROM project3 WHERE Type = ? AND PrimaryBreed = ?", (pet_types[0], primary_breeds[0]))
+        
         # Render the form with the dropdown menus
-        return render_template('dashboard.html', pet_types=pet_types, primary_breeds=primary_breeds, selected_pet_type=selected_pet_type, selected_primary_breed=selected_primary_breed, data=None)
+        return render_template('dashboard.html', pet_types=pet_types, primary_breeds=primary_breeds, selected_pet_type=pet_types[0], selected_primary_breed=primary_breeds[0], data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
