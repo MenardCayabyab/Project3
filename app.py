252b3c50b23eff44da1245ac18b43ff5a64eb37e
Menard <cayabyab.menard@ymail.com>
2023-04-19 17:24:07 -0700

    Added Dyanimc Bar Chart

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request
+from collections import Counter
 import sqlite3
 
 app = Flask(__name__)
 
-DATABASE = './resources/Project3.db'
+DATABASE = 'Project3.db'
 
 def query_database(query, args=()):
     conn = sqlite3.connect(DATABASE)
@@ -13,6 +14,10 @@ def query_database(query, args=()):
     cur.close()
     conn.close()
     return rows
+
+def count_outcome_types(data):
+    outcome_types = [row[13] for row in data if row[13] is not None]
+    return dict(Counter(outcome_types))
 
 @app.route('/', methods=['GET', 'POST'])
 def dashboard():
@@ -29,6 +34,9 @@ def dashboard():
         
         # Retrieve the matching data from the database
         data = query_database("SELECT ID, Name, Color, Sex, Size, DateOfBirth, ImpoundNumber, KennelNumber, AnimalID, IntakeDate, DaysInShelter, IntakeType, IntakeSubtype, OutcomeType, OutcomeSubtype, IntakeCondition, OutcomeCondition, IntakeJurisdiction, OutcomeJurisdiction, OutcomeZipCode, SecondaryBreed FROM project3 WHERE Type = ? AND PrimaryBreed = ?", (pet_type, primary_breed))
+
+        # Count the occurrences of each outcome type
+        outcome_types_distribution = count_outcome_types(data)
         
         # Set the selected options for the dropdown menus
         selected_pet_type = pet_type
@@ -38,7 +46,7 @@ def dashboard():
         primary_breeds = [row[0] for row in query_database("SELECT DISTINCT PrimaryBreed FROM project3 WHERE Type = ?", (pet_type,))]
         
         # Render the template with the matching data
-        return render_template('dashboard.html', pet_types=pet_types, primary_breeds=primary_breeds, selected_pet_type=selected_pet_type, selected_primary_breed=selected_primary_breed, data=data)
+        return render_template('dashboard.html', pet_types=pet_types, primary_breeds=primary_breeds, selected_pet_type=selected_pet_type, selected_primary_breed=selected_primary_breed, data=data, outcome_types_distribution=outcome_types_distribution)
     else:
         # Get the list of primary breeds for the default pet type
         primary_breeds = [row[0] for row in query_database("SELECT DISTINCT PrimaryBreed FROM project3 WHERE Type = ?", (pet_types[0],))]
@@ -46,8 +54,11 @@ def dashboard():
         # Retrieve the matching data from the database for the default pet type and primary breed
         data = query_database("SELECT ID, Name, Color, Sex, Size, DateOfBirth, ImpoundNumber, KennelNumber, AnimalID, IntakeDate, DaysInShelter, IntakeType, IntakeSubtype, OutcomeType, OutcomeSubtype, IntakeCondition, OutcomeCondition, IntakeJurisdiction, OutcomeJurisdiction, OutcomeZipCode, SecondaryBreed FROM project3 WHERE Type = ? AND PrimaryBreed = ?", (pet_types[0], primary_breeds[0]))
         
+        # Calculate outcome_types_distribution
+        outcome_types_distribution = count_outcome_types(data)
+
         # Render the form with the dropdown menus
-        return render_template('dashboard.html', pet_types=pet_types, primary_breeds=primary_breeds, selected_pet_type=pet_types[0], selected_primary_breed=primary_breeds[0], data=data)
+        return render_template('dashboard.html', data=data, pet_types=pet_types, primary_breeds=primary_breeds, selected_pet_type=selected_pet_type, selected_primary_breed=selected_primary_breed, outcome_types_distribution=outcome_types_distribution)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ project/templates/outcome_types_chart.js
@@ -0,0 +1,38 @@
+function renderChart(outcomeTypesDistribution) {
+  // Create an SVG element for the bar chart
+  const svg = d3.select("#outcome-types-chart")
+    .append("svg")
+    .attr("width", 600)
+    .attr("height", 300);
+
+  // Create x and y scales
+  const x = d3.scaleBand()
+    .domain(Object.keys(outcomeTypesDistribution))
+    .range([0, 600])
+    .padding(0.1);
+
+  const y = d3.scaleLinear()
+    .domain([0, d3.max(Object.values(outcomeTypesDistribution))])
+    .range([300, 0]);
+
+  // Create and append the bars
+  svg.selectAll(".bar")
+    .data(Object.entries(outcomeTypesDistribution))
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", d => x(d[0]))
+    .attr("y", d => y(d[1]))
+    .attr("width", x.bandwidth())
+    .attr("height", d => 300 - y(d[1]))
+    .attr("fill", "steelblue");
+
+  // Create and append the x-axis
+  svg.append("g")
+    .attr("transform", "translate(0," + 300 + ")")
+    .call(d3.axisBottom(x));
+
+  // Create and append the y-axis
+  svg.append("g")
+    .call(d3.axisLeft(y));
+}
+
