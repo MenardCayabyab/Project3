7d90392490ab79dacc406ea0344fdda9ed3c2556
Menard <cayabyab.menard@ymail.com>
2023-04-20 12:37:27 -0700

    Dynamic scatter plot

--- project/app.py
+++ project/app.py
@@ -1,12 +1,10 @@
-from flask import Flask, render_template, request, jsonify
-import json
-from datetime import datetime
+from flask import Flask, render_template, request
 from collections import Counter
-
 import sqlite3
+from datetime import datetime
+import calendar
 
 app = Flask(__name__)
-
 DATABASE = 'Project3.db'
 
 def query_database(query, args=()):
@@ -19,51 +17,68 @@ def query_database(query, args=()):
     conn.close()
     return rows
 
-
 def count_outcome_types(data):
     outcome_types = [row[13] for row in data if row[13] is not None]
     return dict(Counter(outcome_types))
+
+def calculate_age(dob):
+    if dob is None:
+        return None
+    dob = datetime.strptime(dob, "%Y-%m-%d")
+    now = datetime.now()
+    age = now.year - dob.year - ((now.month, now.day) < (dob.month, dob.day))
+    return age
 
 @app.route('/', methods=['GET', 'POST'])
 def dashboard():
     selected_pet_type = ''
     selected_primary_breed = ''
-    
+    breed_filter = request.args.get('breed_filter')
+
     # Get the list of pet types
     pet_types = [row[0] for row in query_database("SELECT DISTINCT Type FROM project3")]
-    
+
+    # Get the default primary breed for the selected pet type
+    default_primary_breed = query_database("SELECT DISTINCT PrimaryBreed FROM project3 WHERE Type = ?", (pet_types[0],))[0][0]
+
     if request.method == 'POST':
         # Process the selected options from the dropdown menus
         pet_type = request.form['pet_type']
         primary_breed = request.form['primary_breed']
-        
+
         # Retrieve the matching data from the database
         data = query_database("SELECT ID, Name, Color, Sex, Size, DateOfBirth, ImpoundNumber, KennelNumber, AnimalID, IntakeDate, DaysInShelter, IntakeType, IntakeSubtype, OutcomeType, OutcomeSubtype, IntakeCondition, OutcomeCondition, IntakeJurisdiction, OutcomeJurisdiction, OutcomeZipCode, SecondaryBreed FROM project3 WHERE Type = ? AND PrimaryBreed = ?", (pet_type, primary_breed))
 
         # Count the occurrences of each outcome type
         outcome_types_distribution = count_outcome_types(data)
-        
+
         # Set the selected options for the dropdown menus
         selected_pet_type = pet_type
         selected_primary_breed = primary_breed
-        
+
         # Get the list of primary breeds for the selected pet type
         primary_breeds = [row[0] for row in query_database("SELECT DISTINCT PrimaryBreed FROM project3 WHERE Type = ?", (pet_type,))]
 
         # Calculate scatter_plot_data
-        time_filter = 'month'
-        date_format = "%Y-%m"
-        
         query = '''
-            SELECT strftime(?, IntakeDate) as time_period, COUNT(*) as count
+            SELECT strftime('%Y', IntakeDate) as year, strftime('%m', IntakeDate) as month, COUNT(*) as count
             FROM project3
-            GROUP BY time_period
-            ORDER BY time_period
+            WHERE Type = ? AND PrimaryBreed = ?
+            GROUP BY year, month
+            ORDER BY year, month
         '''
-        result = query_database(query, (date_format,))
-
-        scatter_plot_data = [{'time_period': row['time_period'], 'count': row['count']} for row in result]
-
+        result = query_database(query, (pet_type, primary_breed))
+        scatter_plot_data = [
+        {
+            'age': calculate_age(row['DateOfBirth']),
+            'daysInShelter': row['DaysInShelter'],
+            'Sex': row['Sex'],
+            'Name': row['Name'],
+            'AnimalID': row['AnimalID'],
+            'petType': pet_type,
+            'primaryBreed': primary_breed
+        } for row in data]
+        
         # Render the template with the matching data
         return render_template('dashboard.html', pet_types=pet_types, primary_breeds=primary_breeds, selected_pet_type=selected_pet_type, selected_primary_breed=selected_primary_breed, data=data, outcome_types_distribution=outcome_types_distribution, scatter_plot_data=scatter_plot_data)
     else:
@@ -72,24 +87,25 @@ def dashboard():
         
         # Retrieve the matching data from the database for the default pet type and primary breed
         data = query_database("SELECT ID, Name, Color, Sex, Size, DateOfBirth, ImpoundNumber, KennelNumber, AnimalID, IntakeDate, DaysInShelter, IntakeType, IntakeSubtype, OutcomeType, OutcomeSubtype, IntakeCondition, OutcomeCondition, IntakeJurisdiction, OutcomeJurisdiction, OutcomeZipCode, SecondaryBreed FROM project3 WHERE Type = ? AND PrimaryBreed = ?", (pet_types[0], primary_breeds[0]))
-        
+
         # Calculate outcome_types_distribution
         outcome_types_distribution = count_outcome_types(data)
 
-        time_filter = 'month'
-        date_format = "%Y-%m"
-        
-        query = '''
-            SELECT strftime(?, IntakeDate) as time_period, COUNT(*) as count
-            FROM project3
-            GROUP BY time_period
-            ORDER BY time_period
-        '''
-        result = query_database(query, (date_format,))
+        # Calculate scatter_plot_data
+        scatter_plot_data = [
+            {
+                'age': calculate_age(row['DateOfBirth']),
+                'DaysinShelter': row['DaysInShelter'],
+                'Sex': row['Sex'],
+                'Name': row['Name'],
+                'AnimalID': row['AnimalID']
+            } for row in data
+        ]
 
-        scatter_plot_data = [{'time_period': row['time_period'], 'count': row['count']} for row in result]
+        # Render the form with the dropdown menus and scatter plot
+        return render_template('dashboard.html', data=data, pet_types=pet_types, primary_breeds=primary_breeds,
+                            selected_pet_type=selected_pet_type, selected_primary_breed=selected_primary_breed,
+                            outcome_types_distribution=outcome_types_distribution, scatter_plot_data=scatter_plot_data)
 
-        # Render the form with the dropdown menus
-        return render_template('dashboard.html', data=data, pet_types=pet_types, primary_breeds=primary_breeds, selected_pet_type=selected_pet_type, selected_primary_breed=selected_primary_breed, outcome_types_distribution=outcome_types_distribution, scatter_plot_data=scatter_plot_data)
 if __name__ == '__main__':
     app.run(debug=True)
--- project/static/scatter_plot.js
+++ project/static/scatter_plot.js
@@ -1,63 +1,100 @@
-function drawScatterPlot(data) {
-    // Clear the scatter plot container
-    const container = document.getElementById('scatter-plot');
-    container.innerHTML = '';
-  
-    // Set up the dimensions for the scatter plot
-    const margin = { top: 10, right: 30, bottom: 30, left: 60 },
-      width = 460 - margin.left - margin.right,
-      height = 400 - margin.top - margin.bottom;
-  
-    // Create an SVG element to hold the scatter plot
-    const svg = d3.select('#scatter-plot')
-      .append('svg')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-      .attr('transform', `translate(${margin.left},${margin.top})`);
-  
-    // Set up the scales for the scatter plot
-    const x = d3.scaleLinear()
-      .domain([d3.min(data, d => d.time_period), d3.max(data, d => d.time_period)])
-      .range([0, width]);
-  
-    const y = d3.scaleLinear()
-      .domain([0, d3.max(data, d => d.count)])
-      .range([height, 0]);
-  
-    // Add the x-axis to the scatter plot
-    svg.append('g')
-      .attr('transform', `translate(0,${height})`)
-      .call(d3.axisBottom(x));
-  
-    // Add the y-axis to the scatter plot
-    svg.append('g')
-      .call(d3.axisLeft(y));
-  
-    // Add the data points to the scatter plot
-    svg.selectAll('circle')
-      .data(data)
-      .enter()
-      .append('circle')
-      .attr('cx', d => x(d.time_period))
-      .attr('cy', d => y(d.count))
-      .attr('r', 4)
-      .style('fill', '#69b3a2');
-  }
-
-  function updateScatterPlot() {
-    const timeFilter = document.getElementById('time_filter').value;
-    fetch('/scatter_plot_data?time_filter=' + timeFilter)
-      .then(response => response.json())
-      .then(result => {
-        const scatterPlotData = result.scatter_plot_data;
-        drawScatterPlot(scatterPlotData);
-      });
+function filterData(petType, primaryBreed, scatterPlotData) {
+  return scatterPlotData.filter(d => d.petType === petType && d.primaryBreed === primaryBreed);
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    const initialScatterPlotData = window.scatterPlotData;
-    drawScatterPlot(initialScatterPlotData);
-});
+function drawScatterPlot(petType, primaryBreed, scatterPlotData) {
+  // Clear the existing plot
+  const scatterPlotElement = d3.select("#scatter-plot");
+  scatterPlotElement.selectAll("*").remove();
 
-  
+  const filteredData = filterData(petType, primaryBreed, scatterPlotData);
+
+  // Set the dimensions and margins of the graph
+  const margin = { top: 40, right: 40, bottom: 80, left: 100 },
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+  // Append the svg object to the body of the page
+  const svg = scatterPlotElement
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  // Add X axis label
+  svg.append("text")
+    .attr("class", "x-label")
+    .attr("x", width / 2)
+    .attr("y", height + margin.bottom / 2)
+    .style("text-anchor", "middle")
+    .text("Age")
+    .style("font-size", "14px")
+    .style("font-weight", "bold");
+
+  // Add Y axis label
+  svg.append("text")
+    .attr("class", "y-label")
+    .attr("x", -height / 2)
+    .attr("y", -margin.left / 2)
+    .attr("transform", "rotate(-90)")
+    .style("text-anchor", "middle")
+    .text("Number of Days in Shelter")
+    .style("font-size", "14px")
+    .style("font-weight", "bold");
+
+  // Add X axis
+  const x = d3.scaleLinear().domain([0, d3.max(filteredData, d => d.age)]).range([0, width]);
+  svg.append("g").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x)).selectAll("text").style("font-size", "12px");
+
+  // Add Y axis
+  const y = d3.scaleLinear().domain([0, d3.max(filteredData, d => d.daysInShelter)]).range([height, 0]);
+  svg.append("g").call(d3.axisLeft(y)).selectAll("text").style("font-size", "12px");
+
+  // Add dots
+  svg
+    .append("g")
+    .selectAll("dot")
+    .data(filteredData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => x(d.age))
+    .attr("cy", d => y(d.daysInShelter))
+    .attr("r", 3)
+    .style("fill", "#69b3a2");
+
+  // Add scatter plot title
+  svg
+    .append("text")
+    .attr("class", "scatter-title")
+    .attr("x", width / 2)
+    .attr("y", -30)
+    .attr("text-anchor", "middle")
+    .style("font-size", "20px")
+    .style("fill", "#333")
+    .text("Age vs. Days in Shelter")
+}
+
+// Update the scatter plot based on the selected pet type and primary breed
+function updateScatterPlot() {
+  const petType = document.getElementById("pet_type").value;
+  const primaryBreed = document.getElementById("primary_breed").value;
+  drawScatterPlot(petType, primaryBreed, window.scatterPlotData);
+}
+
+// Load the data and initialize the scatter plot
+d3.csv("scatter_plot_data.csv").then(data => {
+  // Convert numeric values to numbers
+  data.forEach(d => {
+    d.age = +d.age;
+    d.daysInShelter = +d.daysInShelter;
+  });
+
+  // Save the data to the global scope
+  window.scatterPlotData = data;
+
+  // Initialize the scatter plot with the default pet type and primary breed
+  const defaultPetType = document.getElementById("pet_type").value;
+  const defaultPrimaryBreed = document.getElementById("primary_breed").value;
+  drawScatterPlot(defaultPetType, defaultPrimaryBreed, data);
+});
