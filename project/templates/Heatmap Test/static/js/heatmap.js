9ad325f165bbaf3c48c835c8ee089e28e4578e22
knnthchng <neth@Kenneths-Mini.home>
2023-04-20 02:41:30 -0700

    updated sketchbook and heatmap

--- project/templates/Heatmap Test/static/js/heatmap.js
+++ project/templates/Heatmap Test/static/js/heatmap.js
@@ -14,24 +14,33 @@ let myMap = L.map("map", {
 
 let URL = "https://data.sonomacounty.ca.gov/resource/924a-vesw.json"
 
+// How many data points am I feeding in??
+// We should divide it up... perhaps by year/month? 
+// Or maybe just from the previous year?
+// We'll have to look for similar heatmaps...
+
+// ... Does it have to be a heatmap?
+// We could just put up markers instead.
+// Color-code them by animal type?
+
 d3.json(URL).then(function(response){
 
     console.log(response)
-    features = response.features;
+    resp = response;
 
     let heatArray = [];
 
-    for (let i = 0; i < response.length; i++) {
-        let animal = features[i];
-        if (animal) {
-            console.log(animal);
-            heatArray.push([animal.location.latitude, animal.location.longitude]);
+    for (let i = 0; i < 500; i++) {
+        let animal_loc = resp[i].location;
+        if (animal_loc) {
+            console.log(animal_loc);
+            heatArray.push([animal_loc.latitude, animal_loc.longitude]);
         }
     }
 
     let heat = L.heatLayer(heatArray, {
-        radius: 20,
-        blur: 35
+        radius: 35,
+        blur: 10
     }).addTo(myMap);
 
 });
